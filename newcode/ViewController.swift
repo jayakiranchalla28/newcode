c20fc66bf03d3125ee96a4c0c6da932a4c5c8f03
jaya <jayakiran.challa@gmail.com>
2018-04-27 14:03:30 +0530

    Initial Commit

---
+++ newcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newcode
+//
+//  Created by wifin_imac on 4/27/18.
+//  Copyright © 2018 wifin_imac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
